bb19e85fcfda4688b290c7d4e596647a5901987e
Tobias Veselsky <veselsky@tu-berlin.de>
2025-01-20 19:49:32 +0100

    Now the TELEGRAM_BOT_TOKEN gets again read from the .env file

--- config/config.py
+++ config/config.py
@@ -1,4 +1,10 @@
+import os
+from dotenv import load_dotenv
+
+# Load environment variables from .env file
+load_dotenv()
+
 class Config:
     CHECK_INTERVAL = 10
     OIOIOI_BASE_URL = "https://algeng.inet.tu-berlin.de"
-    TELEGRAM_BOT_TOKEN = ""
+    TELEGRAM_BOT_TOKEN = os.getenv("TELEGRAM_BOT_TOKEN", "")
